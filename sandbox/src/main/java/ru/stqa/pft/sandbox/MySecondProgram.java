1d60a5d3a6572caa3da8dd08f96ce61fbbb24079
zevgenia <zatonskaya@yandex.ru>
2017-08-23 00:56:11 +0300

    Изменение автотеста Point с учетом точности получаемого рез-та

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MySecondProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MySecondProgram.java
@@ -9,11 +9,14 @@ public class MySecondProgram {
 
     Point p1 = new Point(5, 2);
     Point p2 = new Point(10, 12);
-    System.out.println("Расстояние между двумя точками с координатами " + "А(" + p1.x + ";" + p1.y + ") и В(" + p2.x +
-            ";" + p2.y + ") = " + p2.distance( p1));
 
-//    NumberFormat formatter = NumberFormat.getNumberInstance();
-//    String s = formatter.format(p2.distance(p1));
+    NumberFormat formatter = NumberFormat.getNumberInstance();
+    String s = formatter.format(p2.distance(p1));
+
+    System.out.println("Расстояние между двумя точками с координатами " + "А(" + p1.x + ";" + p1.y + ") и В(" + p2.x +
+            ";" + p2.y + ") = " + s);
+
+//
 //    System.out.println(s);
 
 //    NumberFormat formatter = NumberFormat.getNumberInstance();
