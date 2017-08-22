a17d201d236dd22493ad251689a78bfe51868c14
zevgenia <zatonskaya@yandex.ru>
2017-08-22 23:41:55 +0300

    Создание автотеста Point для расчета расстояния между двумя точками

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MySecondProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MySecondProgram.java
@@ -1,5 +1,8 @@
 package ru.stqa.pft.sandbox;
 
+import java.text.NumberFormat;
+import java.util.Formatter;
+
 public class MySecondProgram {
 
   // public static void main(String[] args) {
@@ -24,8 +27,18 @@ public class MySecondProgram {
 
     Point p1 = new Point(5, 2);
     Point p2 = new Point(10, 12);
-    System.out.println( "Расстояние между двумя точками с координатами " + "А("+ p1.x + ";" + p1.y + ") и В("+ p2.x +
+    System.out.println("Расстояние между двумя точками с координатами " + "А(" + p1.x + ";" + p1.y + ") и В(" + p2.x +
             ";" + p2.y + ") = " + p2.distance( p1));
+
+//    NumberFormat formatter = NumberFormat.getNumberInstance();
+//    String s = formatter.format(p2.distance(p1));
+//    System.out.println(s);
+
+//    NumberFormat formatter = NumberFormat.getNumberInstance();
+//    formatter.setMaximumFractionDigits(2);
+//    formatter.setMaximumIntegerDigits (6);
+//    String r = formatter.format (p2.distance( p1));
+//    System.out.println(r);
 
   }
 
---
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -0,0 +1,18 @@
+package ru.stqa.pft.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PointTests {
+
+  @Test
+  public void testDistance () {
+
+    Point p1 = new Point (5,2);
+    Point p2 = new Point (10,12);
+
+        Assert.assertEquals(p2.distance (p1), 11.1803);
+
+
+  }
+}
