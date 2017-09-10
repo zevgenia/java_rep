38090837a2b17f2547229ae0e37b3cf1b80fe79d
zevgenia <zatonskaya@yandex.ru>
2017-09-10 20:37:56 +0300

    Добавлена возможность получения списка групп с веб-страницы-исправленно

--- addressbook/src/test/java/ru/stqa/rep/addressbook/tests/GroupModificationTests.java
+++ addressbook/src/test/java/ru/stqa/rep/addressbook/tests/GroupModificationTests.java
@@ -17,13 +17,13 @@ public class GroupModificationTests extends TestBase {
       app.getGroupHelper().createGroup(new GroupData("Друзья", "Друзья", "Домашняя группа"));
     }
     List<GroupData> before = app.getGroupHelper().getGroupList();
-    app.getGroupHelper().selectGroup(0);
+    app.getGroupHelper().selectGroup(before.size()-1);
     app.getGroupHelper().initGroupModification();
     app.getGroupHelper().fillGroupForm(new GroupData("Друзья", null, null));
     app.getGroupHelper().submitGroupModification();
     app.getGroupHelper().returnToGroupPage();
     List<GroupData> after = app.getGroupHelper().getGroupList();
-    Assert.assertEquals(after.size(), before);
+    Assert.assertEquals(after.size(), before.size());
     System.out.println("Было: " + before.size() + " Стало: " + after.size());
   }
 
