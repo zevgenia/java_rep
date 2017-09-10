73627d7a0440508214987fc8e2b83baacfb58f3c
zevgenia <zatonskaya@yandex.ru>
2017-09-10 20:11:35 +0300

    Добавлена возможность получения списка групп с веб-страницы

--- addressbook/src/test/java/ru/stqa/rep/addressbook/tests/GroupCreationTests.java
+++ addressbook/src/test/java/ru/stqa/rep/addressbook/tests/GroupCreationTests.java
@@ -4,16 +4,17 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.rep.addressbook.model.GroupData;
 
+import java.util.List;
+
 public class GroupCreationTests extends TestBase {
 
   @Test
   public void testGroupCreation() {
     app.gotoGroupPage();
-    int before = app.getGroupHelper().getGroupCount();
+    List<GroupData> before = app.getGroupHelper().getGroupList();
     app.getGroupHelper().createGroup(new GroupData("Друзья", "Друзья", "Домашняя группа"));
-    int after = app.getGroupHelper().getGroupCount();
-    Assert.assertEquals(after, before + 1);
-    System.out.println("Было: "+before+ " Стало: "+after);
+    List<GroupData> after = app.getGroupHelper().getGroupList();
+    Assert.assertEquals(after.size(), before.size() + 1);
+    System.out.println("Было: "+before.size()+ " Стало: "+after.size());
   }
-
 }
--- addressbook/src/test/java/ru/stqa/rep/addressbook/tests/GroupDeletionTests.java
+++ addressbook/src/test/java/ru/stqa/rep/addressbook/tests/GroupDeletionTests.java
@@ -4,6 +4,8 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.rep.addressbook.model.GroupData;
 
+import java.util.List;
+
 public class GroupDeletionTests extends TestBase {
 
   @Test
@@ -12,13 +14,13 @@ public class GroupDeletionTests extends TestBase {
     if (! app.getGroupHelper().isThereAGroup()){
       app.getGroupHelper().createGroup(new GroupData("Друзья", "Друзья", "Домашняя группа") );
     }
-    int before = app.getGroupHelper().getGroupCount();
-    app.getGroupHelper().selectGroup(before-1);
+    List<GroupData> before = app.getGroupHelper().getGroupList();
+    app.getGroupHelper().selectGroup(before.size()-1);
     app.getGroupHelper().deleteSelectedGroups();
     app.getGroupHelper().returnToGroupPage();
-    int after = app.getGroupHelper().getGroupCount();
-    Assert.assertEquals(after, before-1);
-    System.out.println("Было: "+before+ " Стало: "+after);
+    List<GroupData> after = app.getGroupHelper().getGroupList();
+    Assert.assertEquals(after.size(), before.size()-1);
+    System.out.println("Было: "+before.size()+ " Стало: "+after.size());
 
   }
 }
