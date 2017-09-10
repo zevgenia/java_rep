bc6ba88963059175c8372b92b1de11acd39c8ec8
zevgenia <zatonskaya@yandex.ru>
2017-09-10 20:17:08 +0300

    Revert "Revert "Добавлена возможность получения списка групп с веб-страницы""
    This reverts commit e75d19d52ac1427797595682a1a1e428ea45fd96.

--- addressbook/src/test/java/ru/stqa/rep/addressbook/tests/GroupModificationTests.java
+++ addressbook/src/test/java/ru/stqa/rep/addressbook/tests/GroupModificationTests.java
@@ -4,6 +4,8 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.rep.addressbook.model.GroupData;
 
+import java.util.List;
+
 public class GroupModificationTests extends TestBase {
 
   @Test
@@ -14,15 +16,15 @@ public class GroupModificationTests extends TestBase {
     if (!app.getGroupHelper().isThereAGroup()) {
       app.getGroupHelper().createGroup(new GroupData("Друзья", "Друзья", "Домашняя группа"));
     }
-    int before = app.getGroupHelper().getGroupCount();
+    List<GroupData> before = app.getGroupHelper().getGroupList();
     app.getGroupHelper().selectGroup(0);
     app.getGroupHelper().initGroupModification();
     app.getGroupHelper().fillGroupForm(new GroupData("Друзья", null, null));
     app.getGroupHelper().submitGroupModification();
     app.getGroupHelper().returnToGroupPage();
-    int after = app.getGroupHelper().getGroupCount();
-    Assert.assertEquals(after, before);
-    System.out.println("Было: " + before + " Стало: " + after);
+    List<GroupData> after = app.getGroupHelper().getGroupList();
+    Assert.assertEquals(after.size(), before);
+    System.out.println("Было: " + before.size() + " Стало: " + after.size());
   }
 
 }
