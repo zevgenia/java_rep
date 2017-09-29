970b72ed29e048e4b8e0e502aee8d7ac6bbe4b37
zevgenia <zatonskaya@yandex.ru>
2017-09-29 14:18:14 +0300

    Реализован генератор тестовых данных, запись данных в файл

--- addressbook/src/test/java/ru/stqa/rep/addressbook/generators/GroupDataGenerator.java
+++ addressbook/src/test/java/ru/stqa/rep/addressbook/generators/GroupDataGenerator.java
@@ -3,26 +3,33 @@ package ru.stqa.rep.addressbook.generators;
 import ru.stqa.rep.addressbook.model.GroupData;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
 import java.util.ArrayList;
 import java.util.List;
 
 public class GroupDataGenerator {
 
-  public static  void main (String args[]){
+  public static  void main (String args[]) throws IOException {
 
     int count = Integer.parseInt(args[0]);
     File file = new File(args[1]);
 
     List<GroupData> groups = generateGroups(count);
+    System.out.println(new File(".").getAbsolutePath());
     save (groups, file);
 
 
 
   }
 
-  private static List<GroupData>  save(List<GroupData> groups, File file) {
-
-
+  private static void save(List<GroupData> groups, File file) throws IOException {
+    Writer writer = new FileWriter(file);
+    for (GroupData group : groups ){
+      writer.write(String.format("%s;%s;%s\n",group.getName(),group.getHeader(),group.getFooter()));
+    }
+    writer.close();
 
   }
 
@@ -30,7 +37,8 @@ public class GroupDataGenerator {
     List<GroupData> groups = new ArrayList<GroupData>();
     for (int i=0; i < count; i++){
       groups.add(new GroupData().withName(String.format("Группа %s",i))
-              .withHeader(String.format("header %s",i)).withFooter(String.format("footer %s",i)));
+              .withHeader(String.format("header %s",i))
+              .withFooter(String.format("footer %s",i)));
 
     }
     return groups;
