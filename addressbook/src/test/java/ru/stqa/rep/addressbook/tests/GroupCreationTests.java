546253327bc30dade3ffc8c2f1d77677bdb89fbc
zevgenia <zatonskaya@yandex.ru>
2017-10-01 17:05:52 +0300

    Добавлена паратетризация методов для теста создания групп

--- addressbook/src/test/java/ru/stqa/rep/addressbook/tests/GroupCreationTests.java
+++ addressbook/src/test/java/ru/stqa/rep/addressbook/tests/GroupCreationTests.java
@@ -1,30 +1,47 @@
 package ru.stqa.rep.addressbook.tests;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.rep.addressbook.model.GroupData;
 import ru.stqa.rep.addressbook.model.Groups;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTests extends TestBase {
 
-  @Test(enabled = false)
+  @DataProvider ()
+  public Iterator<Object[]> validGroups(){
+    List<Object[]> list = new ArrayList<Object[]>();
+    list.add(new Object[]{"Друзья'", "Друзья", "Домашняя группа"});
+    list.add(new Object[]{"Семья", "Семья", "Родные"});
+    list.add(new Object[]{"Работа", "Работа", "Сослуживцы"});
+    return list.iterator();
 
-  public void testGroupCreation() {
-    app.goTo().groupPage();
-    Groups before = app.group().all();
-    GroupData group = new GroupData().withName("Друзья").withHeader("Друзья").withFooter("Домашняя группа");
-    app.group().create(group);
-    assertThat(app.group().count(),equalTo(before.size() + 1));
-    Groups after = app.group().all();
-    System.out.println("Было: " + before.size() + " Стало: " + after.size());
-
-    assertThat(after, equalTo(
-            before.withAdded(group.withId(after.stream().mapToInt((g)-> g.getId()).max().getAsInt()))));
   }
 
-  @Test
+  @Test (dataProvider = "validGroups")
+
+  public void testGroupCreation(String name, String header, String footer) {
+      GroupData group = new GroupData().withName(name).withHeader(header).withFooter(footer);
+      app.goTo().groupPage();
+      Groups before = app.group().all();
+
+      app.group().create(group);
+      assertThat(app.group().count(),equalTo(before.size() + 1));
+      Groups after = app.group().all();
+      System.out.println("Было: " + before.size() + " Стало: " + after.size());
+
+      assertThat(after, equalTo(
+              before.withAdded(group.withId(after.stream().mapToInt((g)-> g.getId()).max().getAsInt()))));
+    }
+
+
+  @Test(enabled = false)
 
   public void testBadGroupCreation() {
     app.goTo().groupPage();
