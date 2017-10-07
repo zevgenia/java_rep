4b78857dd46039db4d73dbe2439814dd45451a68
zevgenia <zatonskaya@yandex.ru>
2017-10-07 18:19:48 +0300

    Ошибка соединения с БД с помощью Hibernate

---
+++ addressbook/src/test/java/ru/stqa/rep/addressbook/tests/HbConnectionTest.java
@@ -0,0 +1,56 @@
+package ru.stqa.rep.addressbook.tests;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.junit.BeforeClass;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.Test;
+import ru.stqa.rep.addressbook.model.GroupData;
+import java.util.List;
+
+public class HbConnectionTest {
+  Logger logger= LoggerFactory.getLogger(HbConnectionTest.class);
+
+  private SessionFactory sessionFactory;
+
+  @BeforeClass
+
+  protected void setUp() throws Exception {
+    // A SessionFactory is set up once for an application!
+    final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+            .configure() // configures settings from hibernate.cfg.xml
+            .build();
+    try {
+      sessionFactory = new MetadataSources( registry ).buildMetadata().buildSessionFactory();
+    }
+    catch (Exception e) {
+      e.printStackTrace();
+      // The registry would be destroyed by the SessionFactory, but we had trouble building the SessionFactory
+      // so destroy it manually.
+      StandardServiceRegistryBuilder.destroy( registry );
+    }
+  }
+
+
+  @Test
+  public void HbConnectionTest() {
+
+    logger.info("Start test HbConnectionTest");
+
+    Session session = sessionFactory.openSession();
+    session.beginTransaction();
+    List<GroupData> result = session.createQuery( "from GroupData" ).list();
+    for (GroupData group : result) {
+      System.out.println( group);
+    }
+    session.getTransaction().commit();
+    session.close();
+
+
+    logger.info("Stop test HbConnectionTest");
+  }
+}
